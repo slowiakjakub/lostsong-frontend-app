4371c0bdf2c9c28fc0bf870ea72e18064e95a10d
Jakub Slowiak <slowiakjakub@gmail.com>
2023-11-26 18:40:52 +0100

    Fix backend api url

--- src/components/ConnectionTester.jsx
+++ src/components/ConnectionTester.jsx
@@ -5,7 +5,7 @@ function ConnectionTester() {
     const [data, setData] = useState(null);
 
     useEffect(() => {
-        axios.get('http://localhost:3000/api/songs')
+        axios.get('https://lostsong-backend-144649aa504e.herokuapp.com/api/songs')
             .then(response => setData(response.data))
             .catch(error => console.error('Error:', error));
     }, []);
