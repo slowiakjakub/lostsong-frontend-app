90126056845684b7cbafd2258592226135c0771c
Jakub Slowiak <slowiakjakub@gmail.com>
2023-11-26 18:27:21 +0100

    Sample request to backend api

--- src/components/ConnectionTester.jsx
+++ src/components/ConnectionTester.jsx
@@ -1,5 +1,21 @@
+import React, { useEffect, useState } from 'react';
+import axios, { AxiosError } from 'axios';
+
 function ConnectionTester() {
-    return <div>Connection Tester component here!</div>
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        axios.get('http://localhost:3000/api/songs')
+            .then(response => setData(response.data))
+            .catch(error => console.error('Error:', error));
+    }, []);
+
+    return (
+        <div>
+            Connection Tester component here!
+            {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
+        </div>
+    );
 }
 
 export default ConnectionTester;
