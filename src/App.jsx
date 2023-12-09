52f90f931fbe43d76dd277d73951ee2b7eb40311
Jakub Slowiak <slowiakjakub@gmail.com>
2023-12-09 23:11:25 +0100

    Create sidebar with clickable links

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,15 @@ import ConnectionTester from './components/ConnectionTester'
 import DownloadButton from './components/DownloadButton'
 import AppDescription from './components/AppDescription'
 import Home from './components/Home'
-import { Outlet } from 'react-router'
+import { Outlet, Link } from 'react-router-dom'
 
 function App() {
 
   return (
     <>
     <div>Sidebar</div>
+    <Link to={`home`}>Home</Link>
+    <Link to={`about`}>About</Link>
     <Outlet/>
     </>
   )
