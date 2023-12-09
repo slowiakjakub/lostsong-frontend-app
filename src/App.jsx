43b5024cec9caee58c797451b84c2569aedf319b
Jakub Slowiak <slowiakjakub@gmail.com>
2023-12-09 18:42:55 +0100

    Clean up css

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,11 @@ import DownloadButton from './components/DownloadButton'
 import AppDescription from './components/AppDescription'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <a href="https://react.dev" target="_blank">
-          <img src={lostSongLogo} className="logo" alt="React logo" />
-        </a>
+        <img src={lostSongLogo} className="logo" alt="LostSong logo" />
       </div>
       <h1>LostSong!</h1>
       <AppDescription/>
