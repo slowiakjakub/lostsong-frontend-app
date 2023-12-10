b5740e8f99ea2125958698f3e4759cffb8046198
Jakub Slowiak <slowiakjakub@gmail.com>
2023-12-10 23:22:32 +0100

    Add features without styling

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 
+
+const FeatureItem = ({ children }) => (
+    <div className="feature-item">
+      <span className="feature-icon">✔️</span> {/* Replace with icons if you have them */}
+      {children}
+    </div>
+  );
+
 function Features() {
     return (
-        <>
-            <div>Features</div>
-        </>
-    );
+        <div className="features-container">
+          <FeatureItem>Obsługa formatów MP3/WAV.</FeatureItem>
+          <FeatureItem>Rozpoznanie tytułu pliku muzycznego dzięki zewnętrznemu serwisowi AcousticID.</FeatureItem>
+          <FeatureItem>Tagowanie metadanych pliku (Tytuł, Artysta, Album, Data wydania...) na bazie zawartości pliku.</FeatureItem>
+        </div>
+      );
 }
 
 export default Features;
