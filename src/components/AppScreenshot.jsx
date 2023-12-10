61431ee8a99446cd3242f3f2dd7d1a90bc9def35
Jakub Slowiak <slowiakjakub@gmail.com>
2023-12-10 22:20:42 +0100

    Show screenshot on site

--- src/components/AppScreenshot.jsx
+++ src/components/AppScreenshot.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import screenshot from '../assets/AppScreenshot.png';
 
 function AppScreenshot() {
-  return (
-    <>
-      <div>AppScreenshot component</div>
-    </>
-  );
+    return (
+        <div className="screenshot-container">
+          <img src={screenshot} alt="App Screenshot" className="screenshot-image" />
+        </div>
+      );
 }
 
 export default AppScreenshot;
