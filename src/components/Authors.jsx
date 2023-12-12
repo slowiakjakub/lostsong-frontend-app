4af8a36321dbda2c1d1025c18a9874e4a89a48a3
Jakub Slowiak <slowiakjakub@gmail.com>
2023-12-13 00:24:06 +0100

    Add authors

--- src/components/Authors.jsx
+++ src/components/Authors.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import './Authors.css';
 
 function Authors() {
   return (
-    <>
-      <div>Authors</div>
-    </>
+    <div className="author-container">
+      <h1>Autorzy projektu</h1>
+      <ul>
+        <li className="author-item">Jakub Słowiak</li>
+        <li className="author-item">Paweł Iwan</li>
+        <li className="author-item">Artur Jacent</li>
+      </ul>
+    </div>
   );
 }
 
