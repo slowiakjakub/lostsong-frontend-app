4020b5a4a0987c3e6257af5c4ded2a2466831944
Jakub Slowiak <slowiakjakub@gmail.com>
2023-12-13 02:41:51 +0100

    Repair compiling

--- src/components/DownloadButton.jsx
+++ src/components/DownloadButton.jsx
@@ -5,8 +5,8 @@ function DownloadButton() {
   const handleDownload = () => {
     // Assuming the .exe file is hosted on your server and accessible via a direct link
     const link = document.createElement('a');
-    link.href = 'path_to_your_exe_file.exe'; // Replace with your actual .exe file path
-    link.setAttribute('download', 'filename.exe'); // Optional: Set the download file name
+    link.href = './assets/LostSongInstaller.msi'; // Replace with your actual .exe file path
+    link.setAttribute('download', 'LostSongInstaller.msi'); // Optional: Set the download file name
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
